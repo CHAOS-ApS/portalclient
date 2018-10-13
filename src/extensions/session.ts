5df1ec867ab1a7d0a0d7d2091c890aeba96bbec2
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-10-13 02:32:24 +0200

    Fixed case.

--- src/extensions/session.ts
+++ src/extensions/session.ts
@@ -4,10 +4,10 @@ import {ISession} from "../data"
 import Extension from "./extension"
 
 export default class Session extends Extension {
-	protected readonly extensionName: string = "Session"
+	protected readonly extensionName: string = "session"
 
 	public Create(): ServiceCall<ISession> {
-		const call = this.call<ISession>("Create", null, HttpMethod.Get, SessionRequirement.none)
+		const call = this.call<ISession>("create", null, HttpMethod.Get, SessionRequirement.none)
 
 		call.response.then(r => {
 			this.client.updateSession(r.Body!.Results[0])
@@ -20,7 +20,7 @@ export default class Session extends Extension {
 declare module "../portalClient" {
 	// tslint:disable-next-line
 	interface PortalClient {
-		Session: Session
+		session: Session
 	}
 }
 
