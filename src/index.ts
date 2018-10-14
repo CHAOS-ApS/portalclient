aa6b23bd474023ea38923c2e06d6f3745b954d81
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-10-15 01:05:57 +0200

    Added some exports.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {PortalClient} from "./portalClient"
+import Extension from "./extensions/Extension"
+import ExtensionHandler from "./extensions/ExtensionHandler"
+import {HttpMethod, IServiceCall, SessionRequirement} from "./serviceCall"
 import "./extensions/session"
 
+export {Extension, ExtensionHandler, HttpMethod, IServiceCall, SessionRequirement}
 export default PortalClient
