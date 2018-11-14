9d4e204910b301d0d7c3cfe9ec314d8124325848
Jacob Poul Richardt <jacob@poulrichardt.dk>
2018-11-14 13:46:57 +0100

    Updated oauth return values.

--- src/extensions/oauth.ts
+++ src/extensions/oauth.ts
@@ -1,17 +1,22 @@
 import {HttpMethod, IServiceCall, SessionRequirement} from "../serviceCall"
 import {AuthenticationExtension} from "./extension"
-import {IBooleanResult} from "../data"
-import {IUser} from "./user"
+import {ISession} from "../data"
 
 export default class OAuth extends AuthenticationExtension {
 	public readonly authenticationType = "OAuth"
 	protected readonly extensionName = "OAuth"
 
-	public getLoginEndPoint(callbackUrl: string): IServiceCall<IUser> {
+	public getLoginEndPoint(callbackUrl: string): IServiceCall<ILoginEndPoint> {
 		return this.call("GetLoginEndPoint", {callbackUrl}, HttpMethod.Get, SessionRequirement.basic)
 	}
 
-	public processLogin(callbackUrl: string, responseUrl: string, stateCode: string): IServiceCall<IBooleanResult> {
+	public processLogin(callbackUrl: string, responseUrl: string, stateCode: string): IServiceCall<ISession> {
 		return this.setAuthenticatedOnSuccess(this.call("ProcessLogin", {callbackUrl, responseUrl, stateCode}, HttpMethod.Post, SessionRequirement.basic))
 	}
+}
+
+export interface ILoginEndPoint
+{
+	Uri: string;
+	StateCode: string;
 }
