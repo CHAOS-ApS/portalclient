534fb77e89706038bdde8b64d0a092547fe0d6ab
Jacob Richardt <jacob@poulrichardt.dk>
2018-12-07 11:40:29 +0100

    Fixed casing.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {PortalClient} from "./portalClient"
-import Extension, {AuthenticationExtension} from "./extensions/Extension"
-import ExtensionHandler from "./extensions/ExtensionHandler"
+import Extension, {AuthenticationExtension} from "./extensions/extension"
+import ExtensionHandler from "./extensions/extensionHandler"
 import {HttpMethod, IServiceCall, SessionRequirement} from "./serviceCall"
 
 export {AuthenticationExtension, Extension, ExtensionHandler, HttpMethod, IServiceCall, SessionRequirement}
