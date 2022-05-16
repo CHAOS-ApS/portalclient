5fb3086692bfdf0342ef8d87cd376fb0e53b56b1
Jacob Poul Richardt <jacob@poulrichardt.dk>
2022-05-16 12:24:35 +0200

    Added hasServiceErrorCode function

--- src/errorCode.ts
+++ src/errorCode.ts
@@ -1,7 +1,20 @@
+import {ServiceError} from "./index"
+
 enum ErrorCode {
 	externalError = "EC-0",
 	missingParameter = "EC-99999",
 	unexpectedError = "EC-999999999"
+}
+
+export function hasServiceErrorCode(error: any, ...codes: string[]): boolean {
+	if (!(error instanceof ServiceError))
+		return false
+
+	for (const code of codes)
+		if (error.code === code)
+			return true
+
+	return false
 }
 
 export default ErrorCode
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export {default as Extension} from "./extensions/extension"
 export {default as AuthenticationExtension} from "./extensions/authenticationExtension"
 export {default as ExtensionHandler} from "./extensions/extensionHandler"
 export {HttpMethod, SessionRequirement} from "./data"
-export {default as ErrorCode} from "./errorCode"
+export {default as ErrorCode, hasServiceErrorCode} from "./errorCode"
 export {default as ServiceError} from "./serviceError"
 
 export type {IExtension, IExtensionConstructor} from "./extensions/extension"
